90bb5530b6750c5469d3790ff8fc153b7a886c55
uerbzr <nigel.sibbert@gmail.com>
2024-02-27 18:24:32 +0000

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,37 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import DataList from "./components/DataList.jsx";
 import SelectTypeForm from "./components/SelectTypeForm.jsx";
 import "./App.css";
-
+import { DATA_ENDPOINT } from "./consts.jsx";
 export default function App() {
   const [dataType, setDataType] = useState("");
 
   const [data, setData] = useState(null);
 
+  console.log(dataType);
   console.log({ data });
 
   // Write code here.
   //
 
+  function fn() {
+    fetch(`${DATA_ENDPOINT}/${dataType}/`)
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+      });
+  }
+  useEffect(() => {
+    if (dataType !== "") {
+      fn();
+    } else {
+      setData(null);
+    }
+  }, [dataType]);
+
   return (
     <div>
-      <SelectTypeForm setDataType={setDataType} />
+      <SelectTypeForm setDataType={setDataType} dataType={dataType} />
       {data && <DataList dataType={dataType} data={data.results} />}
     </div>
   );
--- src/components/SelectTypeForm.jsx
+++ src/components/SelectTypeForm.jsx
@@ -1,10 +1,15 @@
 function SelectTypeForm(props) {
   // Write code here...
-
+  const { setDataType, dataType } = props;
   return (
     <form className="three-column-grid__expand-two gap-md">
       <label htmlFor="type">Data Type</label>
-      <select id="type" name="type">
+      <select
+        id="type"
+        name="type"
+        value={dataType}
+        onChange={(e) => setDataType(e.target.value)}
+      >
         <option value="">Please select...</option>
         <option value="people">People</option>
         <option value="planets">Planets</option>
